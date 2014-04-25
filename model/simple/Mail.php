46a855f94849482a3adadac5869ca992bdbfc22d
salorium <salorium@hotmail.fr>
2014-04-25 23:50:05 +0200

    Test Mail

--- model/simple/Mail.php
+++ model/simple/Mail.php
@@ -24,7 +24,7 @@ class Mail extends \core\Model {
        <title>Activation du nouveau mot de passe</title>
       </head>
       <body>
-       <p>Bonjour '.$login.', vous venez de faire une demande de réinitialisation de mot de passe, pour activer ce nouveau mot de passe vous devez cliquer sur le liens en dessous.<br><span style="color:red;"> Si vous n\'avez pas éffectué de réinistialisation de mot de passe, merci de ne pas prendre en compte ce mail.</span></p>
+       <p>Bonjour '.$login.', vous venez de faire une demande de réinitialisation de mot de passe, pour activer ce nouveau mot de passe vous devez cliquer sur le liens en dessous.<br>Si vous n\'avez pas éffectué de réinistialisation de mot de passe, merci de ne pas prendre en compte ce mail.</p>
        <table>
         <tr>
          <td>Mot de passe :</td><td>'.$mdp.'</td>
