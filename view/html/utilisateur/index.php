7f204597dc1594224c7a47b664737434d1beff69
salorium <salorium@hotmail.fr>
2014-04-26 00:44:14 +0200

    Ajout du mail lors de la demande de réinistialisation du mdp

--- view/html/utilisateur/index.php
+++ view/html/utilisateur/index.php
@@ -68,7 +68,7 @@ if (isset ($erreur)){
             <div class="row">
                 <div class="columns">
                     <label>Mot de passe <small>obligatoire</small>
-                    <input name="motdepasse" type="password" required pattern="password" />
+                    <input name="motdepasse" type="password" required pattern="password"  value="<?= (isset($mdp)== true ? $mdp:"") ?>/>
                     </label>
                     <small class="error">Le mot de passe est obligatoire ! (Au moins 8 caractères avec une lettre majuscule, un chiffre / un caractère spécial.) </small>
                 </div>
