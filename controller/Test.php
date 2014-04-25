373c7638c852d437c3991d5e8b596ee83d61b92c
salorium <salorium@hotmail.fr>
2014-04-25 23:01:17 +0200

    Test Mail

--- controller/Test.php
+++ controller/Test.php
@@ -13,6 +13,51 @@ use core\Controller;
 
 
 class Test extends Controller {
+    function mail(){
+        // Plusieurs destinataires
+        $to  = "Geoffrey Saleur <salorium@hotmail.fr>" ;
+
+        // Sujet
+        $subject = 'Calendrier des anniversaires pour Août';
+
+        // message
+        $message = '
+     <html>
+      <head>
+       <title>Calendrier des anniversaires pour Août</title>
+      </head>
+      <body>
+       <p>Voici les anniversaires à venir au mois d\'Août !</p>
+       <table>
+        <tr>
+         <th>Personne</th><th>Jour</th><th>Mois</th><th>Année</th>
+        </tr>
+        <tr>
+         <td>Josiane</td><td>3</td><td>Août</td><td>1970</td>
+        </tr>
+        <tr>
+         <td>Emma</td><td>26</td><td>Août</td><td>1973</td>
+        </tr>
+       </table>
+      </body>
+     </html>
+     ';
+
+        // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+        // En-têtes additionnels
+       // $headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+        $headers .= 'From: Anniversaire <anniversaire@salorium.com>' . "\r\n";
+        //$headers .= 'Cc: anniversaire_archive@example.com' . "\r\n";
+        //$headers .= 'Bcc: anniversaire_verif@example.com' . "\r\n";
+
+        // Envoi
+        $this->set(array(
+            "mail"=>mail($to, $subject, $message, $headers)
+        ));
+    }
     function allocine ( $re){
         $o["typesearch"]="movie";
         $all = new \model\simple\Allocine($re,$o);
