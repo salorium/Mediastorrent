be5ba3c2f68cb3b9eb341decef36da0e38a02e00
salorium <salorium@hotmail.fr>
2014-05-02 10:44:11 +0200

    preparation dl film

--- script/addbibliotheque.php
+++ script/addbibliotheque.php
@@ -60,6 +60,9 @@ if (file_exists($filetorrent)) {
                 case "film":
                     $torrentf = \model\mysql\Torrentfilm::rechercheParNumFileHashClefunique($numfile, $hash, $clefunique);
                     \model\simple\Console::println((is_bool($torrentf) ? "Non Présent" : "Présent"));
+                    if (!is_bool($torrentf)) {
+                        \model\simple\Console::println($torrentf->fini() ? "Sav ok" : "Sav Non ok");
+                    }
                     break;
             }
         }
