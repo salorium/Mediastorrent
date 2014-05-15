d6e27d2d3ac5bef733188b3cb7743d6ae78269c0
salorium <salorium@hotmail.fr>
2014-05-15 16:38:32 +0200

    Update

--- script/initroot.php
+++ script/initroot.php
@@ -26,7 +26,7 @@ function __autoload($class_name)
 exec("chmod a+w " . ROOT . DS . "log");
 exec("chmod a+w " . ROOT . DS . "cache");
 exec("chmod a+w " . ROOT . DS . "config" . DS . "Conf.php");
-exec('echo "php ' . ROOT . DS . "script" . DS . 'cronroot.php &> ' . ROOT . DS . "log" . DS . 'cronroot.log"  >> ' . ROOT . DS . "script" . DS . "cronroot.sh");
+exec('echo "php ' . ROOT . DS . "script" . DS . 'cronroot.php &>> ' . ROOT . DS . "log" . DS . 'cronroot.log\n"  >> ' . ROOT . DS . "script" . DS . "cronroot.sh");
 exec("chmod a+x " . ROOT . DS . "script" . DS . "cronroot.sh");
 exec("crontab -l > mycron");
 exec('echo "*/1 * * * * ' . ROOT . DS . "script" . DS . 'cronroot.sh"  >> mycron');
