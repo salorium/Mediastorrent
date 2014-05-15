806e5e1a451f57733e85882909c9b854843441a8
salorium <salorium@hotmail.fr>
2014-05-15 16:33:17 +0200

    Update

--- script/cronroot.php
+++ script/cronroot.php
@@ -4,4 +4,23 @@
  * User: salorium
  * Date: 15/05/14
  * Time: 16:32
- */ 
+ */
+define('WEBROOT', __DIR__);
+define('ROOT', dirname(WEBROOT));
+define('DS', DIRECTORY_SEPARATOR);
+
+function __autoload($class_name)
+{
+    $filename = ROOT . DS . str_replace("\\", DS, $class_name) . ".php";
+    if (file_exists($filename)) {
+        require_once $filename;
+    } else {
+
+    }
+
+}
+
+//Retour visuel
+\config\Conf::$debuglocalfile = false;
+\model\simple\Console::println("Bonjour");
+?>
