b7b65e2d107029f9a558f1d69e0fb54b30a20d2b
salorium <salorium@hotmail.fr>
2014-05-22 15:10:19 +0200

    Update

--- controller/install/Install.php
+++ controller/install/Install.php
@@ -31,7 +31,6 @@ class Install extends \core\Controller
     function mysqlinit()
     {
         $this->layout = "install";
-        $this->set("res", true);
         /*if (isset($_REQUEST["hostmysql"]) && isset($_REQUEST["loginmysql"]) && isset($_REQUEST["passmysql"])) {
             $querys = file_get_contents(ROOT . DS . "mysql" . DS . "mediastorrent.sql");
             \core\Mysqli::initmultiquery($_REQUEST["hostmysql"], $_REQUEST["loginmysql"], $_REQUEST["passmysql"], $querys);
@@ -39,10 +38,9 @@ class Install extends \core\Controller
 
         } else*/
         if (isset($_REQUEST["login"]) && isset($_REQUEST["pass"]) && isset($_REQUEST["mail"])) {
-            \model\mysql\Utilisateur::insertUtilisateurSysop($_REQUEST["login"], $_REQUEST["pass"], $_REQUEST["mail"]);
+            $res = \model\mysql\Utilisateur::insertUtilisateurSysop($_REQUEST["login"], $_REQUEST["pass"], $_REQUEST["mail"]);
             \model\simple\MakerConf::makerConfEnd();
-        } else {
-
+            $this->set("res", $res);
         }
     }
 
