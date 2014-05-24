7780fce8448765a28c964d021675555d6b9ba933
salorium <salorium@hotmail.fr>
2014-05-24 14:01:18 +0200

    Update

--- view/html/film/normal/nouveau.php
+++ view/html/film/normal/nouveau.php
@@ -5,7 +5,7 @@
  * Date: 22/03/14
  * Time: 03:36
  */
-\core\LoaderJavascript::add("base", "controller.setHost", array($_SERVER["HTTP_HOST"] . dirname(dirname($_SERVER["SCRIPT_NAME"])) . ($_SERVER["SCRIPT_NAME"] !== "/index.php" ? "/" : ""), false))
+\core\LoaderJavascript::add("base", "controller.setHost", array($_SERVER["HTTP_HOST"] . dirname(dirname($_SERVER["SCRIPT_NAME"])) . ($_SERVER["SCRIPT_NAME"] !== "/index.php" ? "/" : ""), $_SERVER["SERVER_PORT"] == 443))
 ?>
 <div class="container">
     <script src="<?= BASE_URL; ?>javascripts/mediastorrent/film.js"></script>
