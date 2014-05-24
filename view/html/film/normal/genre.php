d313e92471a6f860be55aa53a6163dd8433f2fe3
salorium <salorium@hotmail.fr>
2014-05-24 19:07:25 +0200

    Update

---
+++ view/html/film/normal/genre.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salorium
+ * Date: 22/03/14
+ * Time: 03:36
+ */
+\core\LoaderJavascript::add("base", "controller.setHost", array($_SERVER["HTTP_HOST"] . dirname(dirname($_SERVER["SCRIPT_NAME"])) . ($_SERVER["SCRIPT_NAME"] !== "/index.php" ? "/" : ""), $_SERVER["SERVER_PORT"] == 443))
+?>
+<div class="container">
+    <script src="<?= BASE_URL; ?>javascripts/mediastorrent/film.js"></script>
+    <script>
+        // Execution de cette fonction lorsque le DOM sera entièrement chargé
+        $(document).ready(function () {
+            Film.init(<?=json_encode($film)?>);
+        });
+    </script>
+</div>
