6520296e49a38f640f32e3faae2a5487403551e0
salorium <salorium@hotmail.fr>
2014-06-20 23:41:51 +0200

    Update

--- model/simple/Film.php
+++ model/simple/Film.php
@@ -11,5 +11,20 @@ namespace model\simple;
 
 class Film extends \core\Model
 {
+    static function getBackdrop($id)
+    {
+        $backdrop = ROOT . DS . "cache" . DS . "film" . DS . "backdrop";
+        if (!is_dir($backdrop))
+            mkdir($backdrop, 0777, true);
+        if (!file_exists($backdrop . DS . $id . ".jpg")) {
+            var_dump(\model\mysql\Film::getBackdrop($id));
+        }
+        die();
+        return $backdrop . DS . $id . ".jpg";
+    }
 
+    static function getBackdropSetWidth($id, $width)
+    {
+
+    }
 } 
