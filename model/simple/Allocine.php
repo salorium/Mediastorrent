c5ff6aee8cd883b853635080c29513caf0c97d01
salorium <salorium@hotmail.fr>
2014-06-21 14:25:23 +0200

    Update

--- model/simple/Allocine.php
+++ model/simple/Allocine.php
@@ -425,7 +425,7 @@ class Allocine extends Model
             if (isset($films->results)) {
                 if (count($films->results) > 0) {
                     foreach ($films->results as $vvv) {
-                        var_dump(stripos($vvv->original_title, $v->originalTitle));
+                        var_dump($vvv->original_title == $v->originalTitle);
                         if (stripos($vvv->original_title, $v->originalTitle) !== false) {
                             $tmp1 = $tmdb->getMovieImage($vvv->id);
                             foreach ($tmp1->backdrops as $k => $vv) {
