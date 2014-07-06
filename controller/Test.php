3276623fe6e4592dbd396450afbb36429072a6aa
salorium <salorium@hotmail.fr>
2014-07-06 02:15:18 +0200

    Update

--- controller/Test.php
+++ controller/Test.php
@@ -21,6 +21,7 @@ class Test extends Controller
     {
         \config\Conf::$portscgi = $ports;
         var_dump(\model\xmlrpc\rTorrentSettings::get(\config\Conf::$portscgi, true));
+        var_dump(\model\xmlrpc\rXMLRPCRequest::$query);
     }
 
     function addFilm($id)
