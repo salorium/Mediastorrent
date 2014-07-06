dfc2f6abd463bd1b62340871777a62a01ce6c13f
salorium <salorium@hotmail.fr>
2014-07-06 19:20:10 +0200

    Update

--- model/simple/Repertoire.php
+++ model/simple/Repertoire.php
@@ -14,6 +14,10 @@ class Repertoire extends \core\Model
     static function getFindAll()
     {
         $recmd = \model\simple\Console::executeBrut('find /home/' . \config\Conf::$user['user']->login . '/rtorrent/data/ -printf "%p|%s|%y\n" | sort -t \'|\' -k1 | awk -F \'|\' \'{gsub(/\/home\/' . \config\Conf::$user['user']->login . '\/rtorrent\/data\//,"",$1); print "{name:\""$1"\",taille:"$2",type:\""$3"\"}" }\'');
-        var_dump($recmd);
+        if ($recmd[0] !== 0) {
+            trigger_error("Impossible de liste le repertoire de l'utilisateur");
+        }
+        $liste = "[" . implode(",", $recmd[1]) . "]";
+        var_dump($liste);
     }
 } 
