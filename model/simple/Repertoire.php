b5d4428d91e58fa9fca31700761ac3a8aa95d847
salorium <salorium@hotmail.fr>
2014-07-06 19:28:14 +0200

    Update

--- model/simple/Repertoire.php
+++ model/simple/Repertoire.php
@@ -16,8 +16,9 @@ class Repertoire extends \core\Model
         $recmd = \model\simple\Console::executeBrut('find /home/' . \config\Conf::$user['user']->login . '/rtorrent/data/ -printf "%p|%s|%y\n" | sort -t \'|\' -k1 | awk -F \'|\' \'{gsub(/\/home\/' . \config\Conf::$user['user']->login . '\/rtorrent\/data\//,"",$1); print "{name:\""$1"\",taille:"$2",type:\""$3"\"}" }\'');
         if ($recmd[0] !== 0) {
             trigger_error("Impossible de liste le repertoire de l'utilisateur");
+            $recmd[1] = '[]';
         }
         $liste = "[" . implode(",", $recmd[1]) . "]";
-        var_dump($liste);
+        return json_decode($liste, true);
     }
 } 
