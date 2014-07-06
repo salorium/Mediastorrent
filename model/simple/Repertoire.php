e255ab9c165e701e344f28b558b8b7b96c8da9cd
salorium <salorium@hotmail.fr>
2014-07-06 19:43:06 +0200

    Update

--- model/simple/Repertoire.php
+++ model/simple/Repertoire.php
@@ -20,7 +20,7 @@ class Repertoire extends \core\Model
         }
         $liste = "[" . implode(",", $recmd[1]) . "]";
         var_dump($liste);
-        var_dump(json_decode("{" . $liste . "}"));
+        var_dump(json_decode($recmd[1][0]));
         return json_encode($recmd[1][0]);
     }
 } 
