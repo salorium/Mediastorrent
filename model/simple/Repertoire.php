361f78819d7688f4d0d545791a700821638ab231
salorium <salorium@hotmail.fr>
2014-07-06 19:13:58 +0200

    Update

--- model/simple/Repertoire.php
+++ model/simple/Repertoire.php
@@ -13,7 +13,7 @@ class Repertoire extends \core\Model
 {
     static function getFindAll()
     {
-        $recmd = \model\simple\Console::execute('find /dsd/' . \config\Conf::$user['user']->login . '/rtorrent/data/ -printf "%p|%s|%y\n" | sort -t \'|\' -k1 | awk -F \'|\' \'{gsub(/\/home\/' . \config\Conf::$user['user']->login . '\/rtorrent\/data\//,"",$1); print "{name:\""$1"\",taille:"$2",type:\""$3"\"}," }\'');
+        $recmd = \model\simple\Console::execute('find /home/' . \config\Conf::$user['user']->login . '/rtorrent/data/ -printf "%p|%s|%y\n" | sort -t \'|\' -k1 | awk -F \'|\' \'{gsub(/\/home\/' . \config\Conf::$user['user']->login . '\/rtorrent\/data\//,"",$1); print "{name:\""$1"\",taille:"$2",type:\""$3"\"}," }\'');
         var_dump($recmd);
     }
 } 
