e38fe03d8fe5aefac8d290f5d2742254fe8ed1ef
salorium <salorium@hotmail.fr>
2014-07-06 02:04:34 +0200

    Update

--- model/xmlrpc/rTorrent.php
+++ model/xmlrpc/rTorrent.php
@@ -39,4 +39,45 @@ class rTorrent extends \core\Model
         }
         return ($hash);
     }
+
+    static public function fastResume($torrent, $base, $add_path = true)
+    {
+        $files = array();
+        $info = $torrent->info;
+        $psize = intval($info['piece length']);
+        $base = trim($base);
+        if ($base == '') {
+            $req = new rXMLRPCRequest(new \model\xmlrpc\rXMLRPCCommand(\config\Conf::$portscgi, 'get_directory'));
+            if ($req->success())
+                $base = $req->val[0];
+        }
+        if ($psize && \model\xmlrpc\rTorrentSettings::get(\config\Conf::$portscgi)->correctDirectory($base)) {
+            $base = addslash($base);
+            $tsize = 0.0;
+            if (isset($info['files'])) {
+                foreach ($info['files'] as $key => $file) {
+                    $tsize += floatval($file['length']);
+                    $files[] = ($add_path ? $info['name'] . "/" . implode('/', $file['path']) : implode('/', $file['path']));
+                }
+            } else {
+                $tsize = floatval($info['length']);
+                $files[] = $info['name'];
+            }
+            $chunks = intval(($tsize + $psize - 1) / $psize);
+            $torrent->{'libtorrent_resume'}['bitfield'] = intval($chunks);
+            if (!isset($torrent->{'libtorrent_resume'}['files']))
+                $torrent->{'libtorrent_resume'}['files'] = array();
+            foreach ($files as $key => $file) {
+                $ss = LFS::stat($base . $file);
+                if ($ss === false)
+                    return (false);
+                if (count($torrent->{'libtorrent_resume'}['files']) < $key)
+                    $torrent->{'libtorrent_resume'}['files'][$key]['mtime'] = $ss["mtime"];
+                else
+                    $torrent->{'libtorrent_resume'}['files'][$key] = array("priority" => 2, "mtime" => $ss["mtime"]);
+            }
+            return ($torrent);
+        }
+        return (false);
+    }
 }
