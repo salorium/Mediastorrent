ea7dfca64d5a3ad60f4ba6f576ee1192f26b52ee
salorium <salorium@hotmail.fr>
2014-07-08 16:50:30 +0200

    Update

--- webroot/javascripts/mediastorrent/view/torrent1.js
+++ webroot/javascripts/mediastorrent/view/torrent1.js
@@ -861,7 +861,6 @@ Torrent1.view = {
                         e.preventDefault();
                         Torrent1.model.createTorrent.folder.hauteurArbre = list.back;
                         Torrent1.view.createTorrent.afficheArbre();
-                        value = $("#repertoire").val();
                     });
                 }
                 var cpt = 0;
