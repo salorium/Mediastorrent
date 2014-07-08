e5c8f8843ec382f2dc18bfc67275140b6d56d977
salorium <salorium@hotmail.fr>
2014-07-09 01:29:33 +0200

    Update

--- script/fixbrowscap.php
+++ script/fixbrowscap.php
@@ -10,6 +10,6 @@ $content = file('http://browscap.org/stream?q=Full_PHP_BrowsCapINI');
 //var_dump($content);
 foreach ($content as &$row)
     if ($row[0] == '[')
-        $row = str_replace(';', '\;', $row);
+        $row = str_replace(';', '\\;', $row);
 
 file_put_contents('/etc/php/conf.d/browscap.ini', $content);
