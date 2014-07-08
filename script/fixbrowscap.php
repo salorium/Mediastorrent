456dbb92a2136c57a32ec674abef236f46376333
salorium <salorium@hotmail.fr>
2014-07-09 01:20:28 +0200

    Update

---
+++ script/fixbrowscap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salorium
+ * Date: 09/07/14
+ * Time: 01:18
+ */
+$content = file_get_contents('http://browscap.org/stream?q=Full_PHP_BrowsCapINI');
+foreach ($content as &$row)
+    if ($row[0] == '[')
+        $row = str_replace(';', '\\;', $row);
+
+file_put_contents('/etc/php/conf.d/browscap.ini', $content);
