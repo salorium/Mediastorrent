581afb8cbc26db93d67bb11555bfc04ecb3fce26
salorium <salorium@hotmail.fr>
2014-07-08 22:26:56 +0200

    Update

--- script/createtorrent.php
+++ script/createtorrent.php
@@ -32,9 +32,6 @@ $postscgi = $argv[3];
 $req = \core\Memcached::value($utilisateur, "task" . $taskNo);
 if (!is_null($req)) {
     $request = unserialize($req);
-    var_dump($request);
-    exit(1);
-    $comment = '';
     $announce_list = array();
     $trackers = array();
     $trackersCount = 0;
@@ -55,45 +52,26 @@ if (!is_null($req)) {
     }
     if (count($trackers) > 0)
         $announce_list[] = $trackers;
-    $path_edit = trim($request['path_edit']);
-    $piece_size = $request['piece_size'];
+    $path_edit = trim($request["repertoire"]);
+    $piece_size = $request["piece"];
     $callback_log = create_function('$msg', '$fp=fopen("php://stderr","w"); fputs($fp, $msg."\n"); fclose($fp);');
     $callback_err = create_function('$msg', '$fp=fopen("php://stdout","w"); fputs($fp, $msg."\n"); fclose($fp);');
 
     if (count($announce_list) > 0) {
-        $torrent = new Torrent($path_edit, $announce_list[0][0], $piece_size, $callback_log, $callback_err);
+        $torrent = new \model\simple\Torrent($path_edit, $announce_list[0][0], $piece_size, $callback_log, $callback_err);
         if ($trackersCount > 1)
             $torrent->announce_list($announce_list);
     } else
-        $torrent = new Torrent($path_edit, array(), $piece_size, $callback_log, $callback_err);
-
-    if (isset($request['comment'])) {
-        $comment = trim($request['comment']);
-        if (strlen($comment))
-            $torrent->comment($comment);
-    }
+        $torrent = new \model\simple\Torrent($path_edit, array(), $piece_size, $callback_log, $callback_err);
     if (isset($request['private']))
         $torrent->is_private(true);
-    $fname = getUniqueUploadedFilename($torrent->info['name'] . '.torrent');
-    if (isset($request['start_seeding'])) {
-        if (is_dir($path_edit))
-            $path_edit = addslash($path_edit);
-        $path_edit = dirname($path_edit);
-        if ($resumed = rTorrent::fastResume($torrent, $path_edit))
-            $torrent = $resumed;
-        $torrent->save($fname);
-        rTorrent::sendTorrent($torrent, true, true, $path_edit, null, true, isLocalMode());
-        if ($resumed) {
-            if (isset($torrent->{'rtorrent'}))
-                unset($torrent->{'rtorrent'});
-            if (isset($torrent->{'libtorrent_resume'}))
-                unset($torrent->{'libtorrent_resume'});
-            $torrent->save($fname);
-        }
+    if (isset($request["seed"])) {
+        //$path_edit = dirname($path_edit);
+        //$torrent->save($fname);
+        \core\Memcached::value($utilisateur, "torrentfile" . $taskNo, $torrent->__toString(), 60 * 60);
+        \model\xmlrpc\rTorrent::sendTorrent($torrent, true);
     } else
-        $torrent->save($fname);
-    @chmod($fname, $profileMask & 0666);
-    file_put_contents(getTempDirectory() . getUser() . $taskNo . "/out", getFileName($fname));
+        \core\Memcached::value($utilisateur, "torrentfile".$taskNo, $torrent->__toString(), 60 * 60);
     exit(0);
 }
 exit(1);
