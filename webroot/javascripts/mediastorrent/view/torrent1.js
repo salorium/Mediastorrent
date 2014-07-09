1d18ca9e789d4af901c73d5e2d5995cf9b3b7a95
salorium <salorium@hotmail.fr>
2014-07-09 15:57:29 +0200

    Update

--- webroot/javascripts/mediastorrent/view/torrent1.js
+++ webroot/javascripts/mediastorrent/view/torrent1.js
@@ -596,7 +596,7 @@ Torrent1.view = {
                 trackerlist = "";
                 console.log(Torrent1.model.trackersTorrent.liste);
                 $.each(Torrent1.model.trackersTorrent.liste, function (k, v) {
-                    console.log(v);
+                    console.log(v[0]);
                     trackerlist = +v[1] + "\n";
                 });
                 res = ' <form id="updatetracker" method="post" enctype="multipart/form-data" onsubmit="Torrent1.controller.trackersTorrent.edition();">' +
