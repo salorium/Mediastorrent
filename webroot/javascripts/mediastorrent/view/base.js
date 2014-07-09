f0e6c3c4aed14e87ab8ee89f88c8f06fa4eceb16
salorium <salorium@hotmail.fr>
2014-07-09 15:00:47 +0200

    Update

--- webroot/javascripts/mediastorrent/view/base.js
+++ webroot/javascripts/mediastorrent/view/base.js
@@ -17,7 +17,7 @@ Base.view = {
     },
     boxmodal: {
         make: function (titre, contenu) {
-            $loader = $('<div style="background-color: rgba(0,0,0,0.9); position: absolute; top: 0px;left: 0px; bottom: 0px;right: 0px;display: none"><h3 style="color: #ffffff;">Veuillez patienter</h3><span class="loader loader-circles"></span></div>');
+            $loader = $('<div style="background-color: rgba(0,0,0,0.2); position: absolute; top: 0px;left: 0px; bottom: 0px;right: 0px;></div>');
             $("body").append($loader);
         }
     },
