da7ef885ec5dcfef6dc1ddc15ba75fd29cef67b5
salorium <salorium@hotmail.fr>
2014-07-19 19:02:52 +0200

    Ajout de l'infos temps d'attendre dans les films

--- webroot/javascripts/mediastorrent/view/torrent1.js
+++ webroot/javascripts/mediastorrent/view/torrent1.js
@@ -295,6 +295,7 @@ Torrent1.view = {
     },
     filesTorrent: {
         init: function () {
+            console.log("#panel2-2" + $("#panel2-2").height());
             Base.view.fixedHeight("#panel2-2", $("#moitiedroite").height() - Base.model.html.hauteur("#moitiedroite > dl"));
             Base.view.fixedHeight("#panel2-2 > div", $("#panel2-2").height() - Base.model.html.hauteur("#panel2-2 > ul"));
         },
