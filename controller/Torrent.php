68a9a5333b00079bbcff41eb00137b325cff6bc7
salorium <salorium@hotmail.fr>
2014-07-20 03:30:15 +0200

    Update suppression torrent entraine suppression dans la bibliothèque

--- controller/Torrent.php
+++ controller/Torrent.php
@@ -520,10 +520,11 @@ class Torrent extends Controller
         $cmds = array("d.erase");
 
         $req = new \model\xmlrpc\rXMLRPCRequest(\config\Conf::$portscgi);
-        foreach ($_REQUEST["hash"] as $h)
+        foreach ($_REQUEST["hash"] as $h) {
             foreach ($cmds as $cmd)
                 $req->addCommand(new \model\xmlrpc\rXMLRPCCommand(\config\Conf::$portscgi, $cmd, $h));
-        $req->addCommand(new \model\xmlrpc\rXMLRPCCommand(\config\Conf::$portscgi, "d.get_custom", array($h, "clefunique")));
+            $req->addCommand(new \model\xmlrpc\rXMLRPCCommand(\config\Conf::$portscgi, "d.get_custom=", array($h, "clefunique")));
+        }
         $r = ($req->success() ? $req->val : false);
 
         $this->set(array(
