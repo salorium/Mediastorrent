d333d25bbdbf493f608c523cdb55b011f468d3e1
salorium <salorium@hotmail.fr>
2014-07-21 23:51:06 +0200

    Update suppression torrent entraine suppression dans la bibliothèque

--- controller/Test.php
+++ controller/Test.php
@@ -47,6 +47,8 @@ class Test extends Controller
                 if (pathinfo($file["name"], PATHINFO_EXTENSION) != "torrent")
                     $file["name"] .= ".torrent";
                 $des = ROOT . DS . "webroot" . DS . $file["name"];
+                if (file_exists($des))
+                    unlink($des);
                 $torrent['nom'] = $file["name"];
                 $ok = move_uploaded_file($file['tmp_name'], $des);
                 if ($ok) {
