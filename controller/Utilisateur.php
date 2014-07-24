5605c64bdcfe677ac85072a331a97d5d3666433d
salorium <salorium@hotmail.fr>
2014-07-24 02:17:22 +0200

    Update suppression torrent entraine suppression dans la bibliothèque

--- controller/Utilisateur.php
+++ controller/Utilisateur.php
@@ -55,6 +55,17 @@ class Utilisateur extends \core\Controller
         }
     }
 
+    function getKeyconnexion()
+    {
+        if (isset($_COOKIE["keyconnexion"])) {
+            $this->set("key", $_COOKIE["keyconnexion"]);
+        } else {
+            $u = \model\mysql\Utilisateur::authentifierUtilisateurParMotDePasse($_POST["login"], $_POST["motdepasse"]);
+            if (is_object($u))
+                $this->set("key", $u->keyconnexion);
+        }
+    }
+
     function connexionApi()
     {
         //if (!(isset($_POST["login"]) && isset($_POST["motdepasse"])))
