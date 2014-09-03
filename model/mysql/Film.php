3c38f81c544107fc600062ae0091988aff580c67
salorium <salorium@hotmail.fr>
2014-09-04 01:28:49 +0200

    Update

--- model/mysql/Film.php
+++ model/mysql/Film.php
@@ -170,7 +170,7 @@ class Film extends \core\ModelMysql
             select max(tf1.date) from torrentfilm tf1 where tf1.idfilm = tf.idfilm
         )";
         $query .= " and tf.login in (select login from amis a1 where a1.demandeur = " . \core\Mysqli::real_escape_string(\config\Conf::$user["user"]->login) . " and a1.ok = true union select demandeur from amis a2 where a2.login = " . \core\Mysqli::real_escape_string(\config\Conf::$user["user"]->login) . " and a2.ok = true)";
-        $query .= ")  GROUP BY id ORDER BY date DESC";
+        $query .= ") GROUP BY id ORDER BY date DESC";
         \core\Mysqli::query($query);
         return \core\Mysqli::getObjectAndClose(true);
     }
