d103256e6ff04eee1ed3514ec3f0d06af4570e0f
salorium <salorium@hotmail.fr>
2014-09-07 03:09:19 +0200

    Update

--- webroot/index.php
+++ webroot/index.php
@@ -66,7 +66,7 @@ if (\config\Conf::$install) {
 }
 core\Router::connect("Normal", "/", "mediastorrent/accueil");
 core\Router::connect("Torrent", "/", "mediastorrent/accueil");
-core\Router::connect("Sysop", "/", "test/get");
+core\Router::connect("Sysop", "/", "mediastorrent/accueil");
 $Dispa = new core\Dispatcher();
 
 $Dispa->load();
