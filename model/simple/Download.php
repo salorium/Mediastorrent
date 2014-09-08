2c7d67aed37481aa9742581660e529b9f0e23dbf
salorium <salorium@hotmail.fr>
2014-09-09 01:43:31 +0200

    Update

--- model/simple/Download.php
+++ model/simple/Download.php
@@ -15,7 +15,10 @@ class Download extends Model
 {
     static function sendFile($file)
     {
+        var_dump($file);
         $file = htmlspecialchars($file);
+        var_dump($file);
+        die();
         if (!file_exists($file)) {
             throw new \Exception("FILE NOT FOUND");
             exit;
