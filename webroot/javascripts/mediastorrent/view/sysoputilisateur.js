5767e3c7a0ebb9d174ecfb20d8aec0a1bbf51b19
salorium <salorium@hotmail.fr>
2014-09-08 15:46:16 +0200

    Update

--- webroot/javascripts/mediastorrent/view/sysoputilisateur.js
+++ webroot/javascripts/mediastorrent/view/sysoputilisateur.js
@@ -9,15 +9,23 @@ Sysoputilisateur.view = {
         res = ' <form id="ajouterUtilisateur" method="post" enctype="multipart/form-data" onsubmit="Sysoputilisateur.controller();">' +
             '<div class="row expansion">' +
             '    <div class="large-3 columns">' +
-            '<label for="trackers" class="text-center inline">Login : </label>' +
+            '<label for="login" class="text-center inline">Login : </label>' +
             '</div>' +
             '<div class="large-9 columns">' +
-            '    <input type="text" name="login" />' +
+            '    <input type="text" name="login" id="login" />' +
+            '</div>' +
+            '</div>' +
+            '<div class="row expansion">' +
+            '    <div class="large-3 columns">' +
+            '<label for="mail" class="text-center inline">Mail : </label>' +
+            '</div>' +
+            '<div class="large-9 columns">' +
+            '    <input type="text" name="mail" id="mail"/>' +
             '</div>' +
             '</div>' +
             '<div class="row">' +
             '<div class="small-2 small-centered columns">' +
-            '<button class="button small secondary expand" value="create" type="submit">Modifier</button>' +
+            '<button class="button small secondary expand" type="submit">Ajouter</button>' +
             '</div>' +
             '</div></form>';
         Base.view.boxmodal.make("Ajouter un utilisateur", res);
