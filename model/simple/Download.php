5cbd154fed836a25252cc8daecdd9fca4458da0f
salorium <salorium@hotmail.fr>
2014-09-09 01:55:44 +0200

    Update

--- model/simple/Download.php
+++ model/simple/Download.php
@@ -17,8 +17,8 @@ class Download extends Model
     {
         //var_dump($file);
         $file = str_replace("&amp;", "&", $file);
-        var_dump($file);
-        die();
+        //var_dump($file);
+        //die();
         if (!file_exists($file)) {
             throw new \Exception("FILE NOT FOUND");
             exit;
